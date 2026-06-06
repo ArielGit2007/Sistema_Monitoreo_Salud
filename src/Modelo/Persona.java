335ee057a0d4f23593e388e8521af51157d9cb4d
Ariel Arauz <ariel.arauz@udla.edu.ec>
2026-06-05 22:55:05 -0500

    Atributos y metodos de la clase persona

--- src/Modelo/Persona.java
+++ src/Modelo/Persona.java
@@ -1,4 +1,33 @@
 package Modelo;
 
 public class Persona {
+    private String Nombre,Apellido;
+    private int Edad;
+    private double altura, peso;
+    private String genero;
+
+    //Cosntructor de la clase
+    public Persona(String Nombre, String Apellido, int Edad, double altura, double peso, String genero) {
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.Edad = Edad;
+        this.altura = altura;
+        this.peso = peso;
+        this.genero = genero;
+    }
+    //Setters y getters
+
+    public String getNombre() {return Nombre;}
+    public void setNombre(String Nombre) {this.Nombre = Nombre;}
+    public String getApellido() {return Apellido;}
+    public void setApellido(String Apellido) {this.Apellido = Apellido;}
+    public int getEdad() {return Edad;}
+    public void setEdad(int Edad) {this.Edad = Edad;}
+    public double getAltura() {return altura;}
+    public void setAltura(double altura) {this.altura = altura;}
+    public double getPeso() {return peso;}
+    public void setPeso(double peso) {this.peso = peso;}
+    public String getGenero() {return genero;}
+    public void setGenero(String genero) {this.genero = genero;}
+
 }
