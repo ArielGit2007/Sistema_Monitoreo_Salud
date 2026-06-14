39a848ae450eabe6f6ab7971a47830bee27f6cab
Ariel Arauz <ariel.arauz@udla.edu.ec>
2026-06-14 16:59:05 -0500

    Metodo calcular calorias quemadas agregado

--- src/Modelo/actividadFisica.java
+++ src/Modelo/actividadFisica.java
@@ -6,9 +6,31 @@ public class actividadFisica implements Serializable {
 public String tipoActividad;
 public int duracionMinutos;
 public int caloriasQuemadas;
-public int pasosDiarios;
 
+public void CaloriasQuemadas(double peso){
+    double metPorActividad = 0;
 
+    switch(tipoActividad.toLowerCase()) {
+        case "caminar":
+            metPorActividad = 3.5;
+            break;
+        case "correr":
+            metPorActividad = 9.8;
+            break;
+        case "ciclismo":
+            metPorActividad = 8.0;
+            break;
+        case "natacion":
+            metPorActividad = 8.0;
+            break;
+        case "gym":
+            metPorActividad = 6.0;
+            break;
+        default:
+            metPorActividad = 4.0;
+    }
+    this.caloriasQuemadas=(int) (metPorActividad * peso * (duracionMinutos / 60.0));
+}
 
 }
 
