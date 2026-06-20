bfef670d15d7ab62ea02714fa7773a237199d05a
Ariel Arauz <ariel.arauz@udla.edu.ec>
2026-06-19 21:59:48 -0500

    Declaracion inicial de la interfaz

--- src/Interfaz/ventana.java
+++ src/Interfaz/ventana.java
@@ -1,4 +1,19 @@
 package Interfaz;
 
+import javax.swing.*;
+
 public class ventana {
+   public static void main(String[] args) {
+       try {
+           UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+       JFrame Vent = new JFrame("Ventana");
+       Vent.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       Vent.setSize(800, 600);
+       Vent.setVisible(true);
+       ImageIcon icon = new ImageIcon("src/icono-salud.png");
+       Vent.setIconImage(icon.getImage());
+   }
 }
